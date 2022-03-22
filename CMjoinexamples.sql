74faf24b550a2a14b815f5cc8a2737e13bbd0e75
Alex <agarriga2114@gmail.com>
2022-03-22 16:13:30 -0500

    Added CMJoins, moved Join examples sampledb

---
+++ CMjoinexamples.sql
@@ -0,0 +1,38 @@
+#Simple Join Examples using classic models and added tables
+
+#Left Join
+SELECT * FROM employees
+LEFT JOIN usa_company_cars ON employees.employeeNumber = usa_company_cars.PrimaryDriverID;
+
+#Right Join
+SELECT * FROM employees
+RIGHT JOIN usa_company_cars ON employees.employeeNumber = usa_company_cars.PrimaryDriverID;
+
+#Full Join
+SELECT * FROM employees
+LEFT JOIN usa_company_cars ON employees.employeeNumber = usa_company_cars.PrimaryDriverID
+UNION
+SELECT * FROM employees
+RIGHT JOIN usa_company_cars ON employees.employeeNumber = usa_company_cars.PrimaryDriverID;
+
+#Inner Join
+SELECT * FROM employees
+INNER JOIN usa_company_cars ON employees.employeeNumber = usa_company_cars.PrimaryDriverID;
+
+#Cross Join
+SELECT * FROM employees CROSS JOIN usa_company_cars;
+
+#Self Join
+SELECT A.firstName AS firstName1, A.employeeNumber AS employeeNumber1, 
+B.firstName AS firstName2, B.employeeNumber AS employeeNumber2, A.reportsTo
+FROM employees A, employees B
+WHERE A.reportsTo = B.employeeNumber
+ORDER BY A.officeCode;
+
+#Double Inner Join
+Select concat(e.firstName , ' ' , e.lastName) as 'Employee Name' , customerName,
+sum(amount) as "Total Amount Payed by Customer" from employees e
+Inner Join customers c on e.employeeNumber = c.salesRepEmployeeNumber
+Inner Join payments using (customerNumber)
+group by c.customerName
+order by e.employeeNumber;
